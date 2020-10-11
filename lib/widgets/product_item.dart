3eaf61b2b4cd4724a4008f0cf12b68cc00ccb351
willys_tjiawi <willys@huubap.com>
2020-10-11 15:40:23 +0800

    add fading loaded image (show image placeholder when loading image)

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -32,7 +32,11 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          child: Image.network(product.imageUrl, fit: BoxFit.cover),
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(product.imageUrl),
+            fit: BoxFit.cover,
+          ),
           onTap: () {
             Navigator.of(context).pushNamed(
               ProductDetailScreen.routeName,
